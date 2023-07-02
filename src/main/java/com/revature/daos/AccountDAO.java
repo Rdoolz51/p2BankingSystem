dfe3641d7a40c70676fee75399611266ca680fe5
Ryan Dooley <Rdoolz51@aol.com>
2023-07-02 13:24:06 -0400

    extended JPARepository from DAOs, setup a constructor for service layers

--- src/main/java/com/revature/daos/AccountDAO.java
+++ src/main/java/com/revature/daos/AccountDAO.java
@@ -1,7 +1,9 @@
 package com.revature.daos;
 
+import com.revature.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountDAO {
+public interface AccountDAO extends JpaRepository<Account, Integer> {
 }
--- src/main/java/com/revature/daos/TransactionDAO.java
+++ src/main/java/com/revature/daos/TransactionDAO.java
@@ -1,7 +1,9 @@
 package com.revature.daos;
 
+import com.revature.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionDAO {
+public interface TransactionDAO extends JpaRepository<Transaction, Integer> {
 }
