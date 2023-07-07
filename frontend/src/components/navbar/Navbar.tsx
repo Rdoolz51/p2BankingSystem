29ef763f26199a50f2dddcd3e71460e9e45050c2
Ryan Dooley <Rdoolz51@aol.com>
2023-07-07 18:38:56 -0400

    added Logo, fixed profileTab

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import styles from './Navbar.module.css';
 import { FaUser } from "react-icons/fa6";
 import Modal from 'react-modal';
+import Image from 'next/image';
+import pursueLogo from '../../../public/pursueLogo.png';
 
 interface UserProps {
   firstName: string;
@@ -14,17 +16,20 @@ interface UserProps {
 }
 
 
-const Navbar: React.FC<UserProps> = ({firstName, lastName, email, phoneNumber, yearlyIncome}) => {
+const Navbar: React.FC<UserProps> = ({ firstName, lastName, email, phoneNumber, yearlyIncome }) => {
 
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleModal = () => setIsOpen(!isOpen);
-  
+
   return (
     <nav className={styles.nav}>
-      <Link legacyBehavior href={"/"}>
-        <a><h1 className={styles.companyName}>Pursue</h1></a>
-      </Link>
+      <div className={styles.logoContainer}>
+        <Link legacyBehavior href={"/"}>
+          <a><h1 className={styles.companyName}>Pursue</h1></a>
+        </Link>
+        <Image className={styles.logo} src={pursueLogo} alt="Pursue logo" />
+      </div>
       <ul>
         <li>
           <Link legacyBehavior href="/mybank">
@@ -32,14 +37,14 @@ const Navbar: React.FC<UserProps> = ({firstName, lastName, email, phoneNumber, y
           </Link>
         </li>
         <div className={styles.profile}>
-        <li>
-        <div> <a onClick={toggleModal}><FaUser></FaUser> </a> </div>
-        <Modal
-          isOpen={isOpen} 
-          contentLabel="User Information"
-          className={styles.modalContent}
-          overlayClassName={styles.modalOverlay}
-        >
+          <li>
+            <div> <a onClick={toggleModal}><FaUser></FaUser> </a> </div>
+            <Modal
+              isOpen={isOpen}
+              contentLabel="User Information"
+              className={styles.modalContent}
+              overlayClassName={styles.modalOverlay}
+            >
               <h2>User Information</h2>
               <p>First Name: {firstName}</p>
               <p>Last Name: {lastName}</p>
@@ -48,7 +53,7 @@ const Navbar: React.FC<UserProps> = ({firstName, lastName, email, phoneNumber, y
               <p>Yearly Income: ${yearlyIncome}</p>
               <div><a className={styles.signOut} href="/auth/login">Sign Out</a></div>
             </Modal>
-        </li>
+          </li>
         </div>
       </ul>
     </nav>
