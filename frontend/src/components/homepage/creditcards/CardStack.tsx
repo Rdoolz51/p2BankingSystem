e0ecc97750f3d71a331807fcbfb118b89d8145e5
James Johnson <james1q2w3e@gmail.com>
2023-07-08 17:33:18 -0400

    frontend/ was in .gitignore for some reason? Removed it

---
+++ frontend/src/components/homepage/creditcards/CardStack.tsx
@@ -0,0 +1,32 @@
+'use client'
+import styles from './CardStack.module.css'
+import Image from 'next/image';
+import BryanCard from '../../../../public/BryanCard2.jpg'
+
+const CardStack = () => {
+  return (
+    <div className={styles.container}>
+
+      {/* Might come back to transition the images (z-index) better */}
+
+      {/* <div className={styles.imgContainerContainer}> */}
+      <div className={styles.imgContainer}>
+        <Image alt='card' src={BryanCard} />
+        <Image alt='card' src={BryanCard} />
+        <Image alt='card' src={BryanCard} />
+      </div>
+
+      {/* <div className={styles.imgContainer}>
+        <Image alt='card' src={BryanCard} />
+        <Image alt='card' src={BryanCard} />
+      </div>
+      <div className={styles.imgContainer}>
+        <Image alt='card' src={BryanCard} />
+        <Image alt='card' src={BryanCard} />
+      </div> */}
+
+    </div>
+  )
+}
+
+export default CardStack;
