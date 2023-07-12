1b7db0f1ad857f0b5b8e0694773b7a0ece98396a
James Johnson <james1q2w3e@gmail.com>
2023-07-12 19:14:58 -0400

    Transfer between users working. Still working on the 'View their accounts via email' thing

--- frontend/src/components/transfers/NewTransfer.tsx
+++ frontend/src/components/transfers/NewTransfer.tsx
@@ -3,21 +3,6 @@ import styles from './NewTransfer.module.css'
 import { useSession } from "next-auth/react";
 import Modal from "react-modal";
 
-// const fetchAccountByEmail = async (email:String, token:String) => {
-//   try{
-//     const res = await fetch(`${process.env.API_URL}/mybank/${active}`, {
-//       headers: {
-//         Authorization: 'Bearer ' + token,
-//         'Content-Type': 'application/json'
-//       }
-//     }) 
-//       const response = await res.json();
-//       return response;
-//     } catch(e) {
-//       console.log("ERROR" , e)
-//     }
-// }
-
 const submitTransfer = async (data:Object, token:String) => {
   try{
     const res = await fetch(`${process.env.API_URL}/mybank/transfers`, {
@@ -45,7 +30,7 @@ const submitTransfer = async (data:Object, token:String) => {
 
 const submitTransferOther = async (data:Object, token:String) => {
   try{
-    const res = await fetch(`${process.env.API_URL}/mybank/transfers`, {
+    const res = await fetch(`${process.env.API_URL}/transfers`, {
       method: 'POST',
       headers: {
         Authorization: 'Bearer ' + token,
@@ -58,11 +43,11 @@ const submitTransferOther = async (data:Object, token:String) => {
       const data = await res.json();
       return data;
     } else {
-      console.error('Something went wrong (/transfer) ')
+      console.error('Something went wrong other (/transfers) ')
       return null;
     }
   } catch (e) {
-    console.error('Something went wrong doing transfer stuff: ', e)
+    console.error('Something went wrong doing other transfer stuff: ', e)
     return null;
   }
 }
@@ -95,27 +80,27 @@ const NewTransfer = (props:any) => {
     if(res) {
       toggleModal();
     }
-    console.log(data);
   }
 
   //transfers to other accounts
   const handleSubmitOther = async (event:any) => {
+    const intAcc = parseInt(toAccount)
     event.preventDefault();
     const data = {
-      fromAccountId: accountID,
-      toAccountId: toAccount, //hardcoded for now
+      fromEmail: session.data?.user?.email,
+      toEmail: email,
+      fromAcct: accountID,
+      toAcct: intAcc, //hardcoded for now
       amount: `${amount}`
     };
 
     // const res = await fetchAccountByEmail(email, userToken)
 
     const res = await submitTransferOther(data, userToken);
-    console.log('REEEEEEEEEEEEEs >>> ', res);
 
     if(res) {
       toggleModal();
     }
-    console.log(data);
   }
 
   const toggleModal = () => setIsOpen(!isOpen);
@@ -145,7 +130,8 @@ const NewTransfer = (props:any) => {
           ) : (
             <form onSubmit={handleSubmitOther} className={styles.modalForm}>
               <h1 className={styles.formTitle}>Transfer To <strong>Other Accounts</strong></h1>
-              <input type="text" placeholder="Account Number" required onChange={(event) => setToAccount(event.target.value)}/>
+              <input type="text" placeholder="Email" required onChange={(event) => setEmail(event.target.value)}/>
+              <input type="number" placeholder="Account Number" required onChange={(event) => setToAccount(event.target.value)}/>
               <input type="number" placeholder="Amount" onChange={(event) => setAmount(event.target.value)}/>
               <button type="submit" className={styles.submitButton}>Submit</button>
             </form>
