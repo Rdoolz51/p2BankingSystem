8994961698104d79d805fcfc77ab575faa78960f
Ryan Dooley <Rdoolz51@aol.com>
2023-07-12 11:30:15 -0400

    reformatted date on CC

--- frontend/src/components/yourCards/YourCards.tsx
+++ frontend/src/components/yourCards/YourCards.tsx
@@ -15,9 +15,23 @@ interface CardProps {
   }
 }
 
+function formatDate(input: string): string {
+  const date = new Date(input);
+  const month = date.getMonth() + 1;  // months are zero-based in JavaScript
+  const year = date.getFullYear();
+
+  return `${month.toString().padStart(2, '0')}/${year.toString().slice(-2)}`;
+}
+
+
+
 const YourCards: React.FC<CardProps> = (props) => {
   const initialCardBalance = parseInt(props.balance);
   const [balance, setBalance] = useState(initialCardBalance);
+  
+  
+  const inputDate = props.cardExpiration;
+  const outputDate = formatDate(inputDate);
 
 
   const spendMoney = (amount: number) => {
@@ -34,7 +48,7 @@ const YourCards: React.FC<CardProps> = (props) => {
     <div className={styles.card}>
         <h2 className={styles.bankName}>Pursue</h2>
         <h2 className={styles.cardNumber}>**** **** **** {props.cardNumber.substring(12)}</h2>
-        <h2 className={styles.expirationDate}>Exp: {props.cardExpiration}</h2>
+        <h2 className={styles.expirationDate}>Exp: {outputDate}</h2>
     </div>
         <div className={styles.cardLimit}>
         <h2 className={styles.creditLimit}>Credit Limit: ${parseInt(props.creditLimit).toFixed(2)}</h2>
