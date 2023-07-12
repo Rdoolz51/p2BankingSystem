21148919c5e3884abf4e7ff5fea97e7831c0ad55
Ryan Dooley <Rdoolz51@aol.com>
2023-07-12 09:11:56 -0400

    setting up

--- frontend/src/app/bankApplications/page.tsx
+++ frontend/src/app/bankApplications/page.tsx
@@ -3,9 +3,20 @@ import CardApp from "@/components/cardApp/CardApp";
   
   export default async function Page() {
 
+    const appHandler = (num) => {
+      if(){
+
+      }
+    }
+
     return (
-      <main >
-      <CardApp />
+      <main>
+        <button onClick={appHandler(1)}>
+          Apply For A Credit Card
+        </button>
+        <button onClick={appHandler(2)}>
+          Apply For A Loan
+        </button>
       </main>
     );
   }
