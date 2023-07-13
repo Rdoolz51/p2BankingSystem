129a28ac081c689719176dd0cb0476acacf08558
Ryan Dooley <Rdoolz51@aol.com>
2023-07-13 07:10:37 -0400

    mapped card and loan page

--- frontend/src/components/yourCards/YourCards.tsx
+++ frontend/src/components/yourCards/YourCards.tsx
@@ -4,68 +4,88 @@ import styles from './YourCards.module.css';
 import { useSession } from 'next-auth/react';
 
 interface CardProps {
-  interestRate: string;
-  cardNumber: string;
-  balance: string;
-  creditLimit: string;
-  cardExpiration: string;
-  annualFee: string;
-  user: {
-    firstName: string;
-    lastName: string;
-  }
+  cards: {
+    
+    interestRate: string;
+    cardNumber: string;
+    balance: string;
+    creditLimit: string;
+    cardExpiration: string;
+    annualFee: string;
+    user: {
+      firstName: string;
+      lastName: string;
+    }
+}[];
 }
 
-function formatDate(input: string): string {
-  const date = new Date(input);
-  const month = date.getMonth() + 1;  // months are zero-based in JavaScript
-  const year = date.getFullYear();
-
-  return `${month.toString().padStart(2, '0')}/${year.toString().slice(-2)}`;
+const fetchRoutes = async (active, token) => {
+  try{
+    const res = await fetch(`${process.env.API_URL}/mybank/${active}`, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'application/json'
+      }
+    }) 
+      const response = await res.json();
+      return response;
+    } catch(e) {
+      console.log("ERROR" , e)
+    }
 }
 
+// function formatDate(input: string): string {
+//   const date = new Date(input);
+//   const month = date.getMonth() + 1; 
+//   const year = date.getFullYear();
 
+//   return `${month.toString().padStart(2, '0')}/${year.toString().slice(-2)}`;
+// }
 
 const YourCards: React.FC<CardProps> = (props) => {
-  if(!props) {
-    return <div><button>Show CardApp</button> </div>
-  }
-  const initialCardBalance = parseInt(props.balance);
-  const [balance, setBalance] = useState(initialCardBalance);
+
+  const [cardData, setCardData] = useState(Object.values(props));
+
+  console.log("33333333333333333", cardData)
   const session = useSession();
   const userData = session.data?.user;
   
   
-  const inputDate = props.cardExpiration;
-  const outputDate = formatDate(inputDate);
+  // const inputDate = cardData.cardExpiration;
+  // const outputDate = formatDate(inputDate);
 
 
-  const spendMoney = (amount: number) => {
-    if(balance + amount <= parseInt(props.creditLimit)) {
-      setBalance(prevBalance => prevBalance + amount);
-    } else {
-      alert("Credit limit exceeded");
-    }
-  }
+  // const makePayment = (amount: number) => {
+  //   if(balance + amount <= parseInt(cardData.creditLimit)) {
+  //     setBalance(prevBalance => prevBalance + amount);
+  //   } else {
+  //     alert("Credit limit exceeded");
+  //   }
+  // }
 
   return (
-    <>
-    <h2 className={styles.cardType}>Credit Card</h2>
+    <div>
+      {cardData.map((card, index) => (
+        <div key={index} className={styles.divDiv}>
+
+        <h2 className={styles.cardType}>Credit Card</h2>
     <div className={styles.card}>
         <h2 className={styles.bankName}>Pursue</h2>
-        <h2 className={styles.cardNumber}>**** **** **** {props.cardNumber.substring(12)}</h2>
-        <h2 className={styles.expirationDate}>Exp: {outputDate}</h2>
+        <h2 className={styles.cardNumber}>**** **** **** {card.cardNumber.substring(12)}</h2>
+        <h2 className={styles.expirationDate}>Exp: {card.cardExpiration}</h2>
     </div>
         <div className={styles.cardLimit}>
-        <h2 className={styles.creditLimit}>Credit Limit: ${parseInt(props.creditLimit).toFixed(2)}</h2>
+        <h2 className={styles.creditLimit}>Credit Limit: ${parseInt(card.creditLimit).toFixed(2)}</h2>
         </div>
         <div className={styles.cardBalance}>
-        <h2 className={styles.balance}>Balance: ${balance.toFixed(2)}</h2>
+        <h2 className={styles.balance}>Balance: ${parseInt(card.balance).toFixed(2)}</h2>
       </div>
       <div className={styles.buttonContainer}>
-          <div className={styles.button} onClick={() => spendMoney(10)}>Make Payment</div>
+          <div className={styles.button}>Make Payment</div>
         </div>
-    </>
+        </div>
+    ))}
+    </div>
     )
   }
   
--- frontend/src/components/yourLoans/YourLoans.tsx
+++ frontend/src/components/yourLoans/YourLoans.tsx
@@ -1,31 +1,52 @@
+'use client'
+import React, { useState } from "react";
 import styles from './YourLoans.module.css';
 
 
-
 interface LoanProps {
-loanAmount: string;
-type: {
+loans: {
+  loanAmount: string;
+  type: {
     type: string;
-};
-loanBalance: string;
-interestRate: string;
-user: {
+  };
+  loanBalance: string;
+  interestRate: string;
+  user: {
     firstName: string;
     lastName: string;
+  }
+}[]
+  
 }
 
+const fetchRoutes = async (active, token) => {
+  try{
+    const res = await fetch(`${process.env.API_URL}/mybank/${active}`, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+        'Content-Type': 'application/json'
+      }
+    }) 
+      const response = await res.json();
+      return response;
+    } catch(e) {
+      console.log("ERROR" , e)
+    }
 }
 
 const YourLoans: React.FC<LoanProps> = (props) => {
 
+  const [loanData, setLoanData] = useState(Object.values(props));
 
   return (
     <div className={styles.accountContainer}>
+      {loanData.map((loan, index) => (
+        <div key={index} className={styles.divDiv}>
       <div className={styles.leftContainer}>
-        <h2 className={styles.accountType}>{props.user.firstName}'s {props.type.type} Loan</h2>  
-        <h2 className={styles.balance}>Initial Loan Amount: <span>${(parseInt(props.loanAmount).toFixed(2))}</span></h2>
-        <h2 className={styles.balance}>Current Balance: <span>${(parseInt(props.loanBalance).toFixed(2))}</span></h2>
-        <h2 className={styles.balance}>Interest Rate: <span>${(props.interestRate)}%</span></h2>
+        <h2 className={styles.accountType}>{loan.user.firstName}'s {loan.type.type} Loan</h2>  
+        <h2 className={styles.balance}>Initial Loan Amount: <span>${(parseInt(loan.loanAmount).toFixed(2))}</span></h2>
+        <h2 className={styles.balance}>Current Balance: <span>${(parseInt(loan.loanBalance).toFixed(2))}</span></h2>
+        <h2 className={styles.balance}>Interest Rate: <span>${(loan.interestRate)}%</span></h2>
       </div>
       <div className={styles.buttonContainer}>
           <div className={styles.button}>Make Payment</div>
@@ -33,6 +54,8 @@ const YourLoans: React.FC<LoanProps> = (props) => {
       <div className={styles.transactionContainer}>
         {/* <h2>Recent Transactions</h2> */}
       </div>
+      </div>
+    ))}
     </div>
   );
 }
